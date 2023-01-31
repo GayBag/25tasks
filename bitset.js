c695deb19361da1215c6157516c03ed5cb0e06ec
GayBag <111243339+GayBag@users.noreply.github.com>
2023-01-31 22:46:50 +0400

    Add files via upload

---
+++ bitset.js
@@ -0,0 +1,38 @@
+class Bitset {
+	
+	constructor (size)
+	{
+		this.size = size;
+		this.array = [];
+		if (this.size%32)
+		{
+			this.array.length = Math.floor(this.size/32)+1;
+		}
+		else
+		{
+			this.array.length = this.size/32;
+		}
+		this.array.fill(0,0,this.array.length);
+	}
+
+	set (bit)
+	{
+		 let i = Math.floor(bit/32);
+		 this.array[i] ^= (1 << bit);
+	}
+	
+	reset (bit)
+        {
+                 let i = Math.floor(bit/32);
+                 this.array[i] ^= (1 << bit);
+	}
+
+	
+}
+
+let bit_set = new Bitset(65);
+bit_set.set(5);
+bit_set.reset(67);
+
+console.log (bit_set.array[0]);
+
